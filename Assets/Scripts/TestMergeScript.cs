05d5a19e1bf329ed701fb9bf400ad0c891c67a43
Jake McClay <Jake.McClay96@gmail.com>
2019-05-26 11:39:19 +1000

    Addition of Scripts
    Added scripts folder and first test script

---
+++ Assets/Scripts/TestMergeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMergeScript : MonoBehaviour
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
