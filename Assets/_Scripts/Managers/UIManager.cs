3ee00bf73ee18f3638c045d1c1c801e02517b557
Jake McClay <Jake.McClay96@gmail.com>
2019-08-11 21:02:49 +1000

    UI Update
    Added inventory UI

---
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject Inventory;
+    public GameObject closeBtn;
+
+    private bool inventoryOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyUp("b")) {
+            if (inventoryOpen)
+            {
+                Inventory.gameObject.SetActive(false);
+                inventoryOpen = false;
+            }
+            else {
+                Inventory.gameObject.SetActive(true);
+                inventoryOpen = true;
+            }
+            
+        }
+
+
+    }
+
+
+
+    void RemoveInventoryItem() {
+
+
+
+
+
+    }
+}
